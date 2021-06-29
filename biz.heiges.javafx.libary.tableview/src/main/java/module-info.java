ae9009c2d96b27a49df76456eaadc9e54b6394e5
Heiges <hansjoachim.heiges@heiges.biz>
2021-06-29 19:01:46 +0200

    added graphics

--- biz.heiges.javafx.libary.tableview/src/main/java/module-info.java
+++ biz.heiges.javafx.libary.tableview/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module biz.heiges.javafx {
     requires transitive javafx.controls;
+	requires javafx.graphics;
     exports biz.heiges.javafx.libary.commons;
     exports biz.heiges.javafx.libary.tableview;
     exports biz.heiges.javafx.libary.tableview.cell;
